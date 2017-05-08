a63e3effb2fbbdcea0ea3e0d2109f1d244489d71
seanhodges61 <seanhodges@cmail.carleton.ca>
2017-05-08 11:11:04 -0400

    Updated to work with randomly generated exercises

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var path = require('path');
+var pug  = require('pug');
 var express=require('express');
 var app = express();
 const ROOT = "./public";
+
+app.set('view engine', 'pug');
+var ssetExercise = require("./modules/sset.js");
+var ssetPug      = pug.compileFile("./views/sset/sset.pug");
+
+app.get("/ssetExercise", function(req,res){
+	res.status(200).send(ssetExercise.create(ssetPug));
+});
 
 app.use(express.static(ROOT));
 
